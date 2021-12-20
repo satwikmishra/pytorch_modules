e67e0d5494a6cb2c28877a43ef2027a33bcf73f5
satwikmishra <satwiksmishra@gmail.com>
2021-12-20 22:06:33 +0530

    Update CIFAR_10_image_loader.py

--- src/CIFAR_10_image_loader.py
+++ src/CIFAR_10_image_loader.py
@@ -1,5 +1,9 @@
 class ImageDataLoader(DataLoader):
-
+    """
+    Load Image datasets from torch
+    Default category is MNIST
+    Pass category as 'MNIST' or 'CIFAR10'
+    """
 
     def __init__(self, train_transforms, test_transforms, data_dir, batch_size, shuffle, category='custom',
                  num_workers=4, pin_memory=False, device='cpu',
@@ -24,23 +28,25 @@ class ImageDataLoader(DataLoader):
 
         self.classes = None
 
+        
 	self.train_loader = datasets.CIFAR10(
 		self.data_dir,
 		train=True,
 		download=True,
 		transform=get_augmentation(train_transforms)
 		# transform=transforms.build_transforms(train=True)
-	)
+	    )
 	self.test_loader = datasets.CIFAR10(
 		self.data_dir,
 		train=False,
 		download=True,
 		# transform=transforms.build_transforms(train=False)
 		transform=get_augmentation(test_transforms)
-	)
+	    )
 	self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
-					'horse', 'ship', 'truck')
+			    'horse', 'ship', 'truck')
 	self.train_loader = DataLoader(self.train_loader, shuffle=shuffle, **self.init_kwargs)
 	self.test_loader = DataLoader(self.test_loader, **self.init_kwargs)
 
+       
         
