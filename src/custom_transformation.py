7d433107ffb9d73857d5406355b0a9753b670043
satwikmishra <satwiksmishra@gmail.com>
2021-12-20 22:48:28 +0530

    Update custom_transformation.py

--- src/custom_transformation.py
+++ src/custom_transformation.py
@@ -8,4 +8,4 @@ class CIFAR10Transforms:
     def build_transforms(self,  train_list=[], test_list=[]):
         train_list.extend([A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), ToTensorV2()])
         test_list.extend([A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]), ToTensorV2()])
-        return A.Compose(train_tfms_list), A.Compose(test_tfms_list)
+        return A.Compose(train_list), A.Compose(test_list)
